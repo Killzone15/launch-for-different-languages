7965586323ff363394b9b6702aa9a8f2edbb4945
Killzone15 <Killzone15@yandex.ru>
2023-07-18 20:18:42 +0300

    Убрали лишние импорты

--- conftest.py
+++ conftest.py
@@ -1,7 +1,6 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+
 
 
 @pytest.fixture(scope="function")
