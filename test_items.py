c41e1c3b4b2d95fceca49c0d94890a2be65c3ce5
Killzone15 <Killzone15@yandex.ru>
2023-07-21 16:55:24 +0300

    Тест проверки наличия кнопки 'Добавить в корзину'

--- test_items.py
+++ test_items.py
@@ -11,3 +11,4 @@ def test_add_to_cart_button_exists(browser):
     add_to_cart_button = browser.find_element(By.CSS_SELECTOR, '.btn.btn-lg.btn-primary.btn-add-to-basket')
 
     assert add_to_cart_button is not None, "Кнопка добавления в корзину не найдена на странице товара!"
+
